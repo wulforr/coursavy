4e19ce4b15b37e68133602150f310884a382efee
root <root@localhost.localdomain>
2019-12-22 00:02:45 +0530

    added login styles

--- src/CustomDialog.js
+++ src/CustomDialog.js
@@ -7,17 +7,12 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DateFnsUtils from '@date-io/date-fns';
-import {
-    MuiPickersUtilsProvider,
-    KeyboardDatePicker,
-  } from '@material-ui/pickers';
-
-
-  import Radio from '@material-ui/core/Radio';
-  import RadioGroup from '@material-ui/core/RadioGroup';
-  import FormControlLabel from '@material-ui/core/FormControlLabel';
-  import FormControl from '@material-ui/core/FormControl';
-  import FormLabel from '@material-ui/core/FormLabel';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 
 export default class CustomDialog extends Component {
--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import cookie from 'react-cookies'
 import axios from 'axios'
 import moment from 'moment';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+
 
 class Login extends Component {
     constructor(){
         super()
         this.state = {
             token:cookie.load('token'),
+            email:'',
+            password:''
             // cookieloaded:false
         }
     }
-    login = () => {
+
+    handleOnChange = field => event => {
+        console.log(event,field)
+        this.setState({[field]: event.target.value})
+      }
+    
+    handlelogin = () => {
         console.log("in login")
         axios.post('https://reqres.in/api/login',{
             "email": "eve.holt@reqres.in",
@@ -28,23 +40,60 @@ class Login extends Component {
         })
         .catch(err => console.log(err))
     }
-    logout = () => {
+    handlelogout = () => {
         cookie.remove('token',{path:'/'})
         this.setState({token:undefined})
     }
 
     render() {
         console.log(this.state)
-        const day = moment("1995-12-25");
-        console.log(day.year(),moment().diff(moment('2008-08-14T23:22:45.210Z'), 'years'))
+        // const day = moment("1995-12-25");
+        // console.log(day.year(),moment().diff(moment('2008-08-14T23:22:45.210Z'), 'years'))
         if(this.state.token){
-            return(<button onClick={this.logout}>logout</button>)
+            return(
+                <div className="login-page">
+                    <div className="logout-btn">
+                <button onClick={this.handlelogout}>logout</button>
+                </div>
+                </div>)
         }
         else {
         return (
-            <div>
-           Login 
-           <button onClick={this.login}>login</button>    
+            <div className="login-page">
+                <div className="login-form">
+
+           {/* Login  */}
+           {/* <button onClick={this.login}>login</button>     */}
+           <div className="text-field">
+           <TextField
+                  id="name"
+                  label="Email"
+                  className="text-field"
+                  value={this.state.email}
+                  onChange={this.handleOnChange("email")}
+                  fullWidth
+                  helperText="Test email - eve.holt@reqres.in"
+                  variant="outlined"
+                  />
+                  test
+                  </div>
+            <div className="text-field">
+            <TextField
+                  id="name"
+                  label="Password"
+                  type="password"
+                  className="text-field"
+                  value={this.state.password}
+                  onChange={this.handleOnChange("password")}
+                  fullWidth
+                  helperText="Test Password - cityslicka"
+                  variant="outlined"
+                  />
+                  </div>
+            <Button  variant="outlined" onClick={this.handlelogin} color="primary">
+                  Login
+                </Button>
+                </div>
             </div>
         );
         }
